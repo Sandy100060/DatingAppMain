fa9ea1ec371d780d4a24e36839d1266cae6a2be1
Sandeep Kumar Yadav <sandeepkumaryadav@Sandeeps-MacBook-Air.local>
2019-01-01 13:52:27 +0530

    Comment

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -26,10 +26,10 @@ namespace DatingApp.API
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        { 
+        {  
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddCors();
+            services.AddCors(); //Allows Angular App to make requests.
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
